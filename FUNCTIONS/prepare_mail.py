4c0e3f93ade11cd8544212f43aefcce03f21d7f4
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2023-10-30 22:47:26 +0200

    Add email preparation function to 'prepare_mail.py'
    The 'run' function was added to a new 'prepare_mail.py' file. This function prepares and sends an email using environment variables ("main_mail", "store_mail" and "subject") for recipients and server details. The email body includes a HTML content body and an attachment file. This functionality was added to automate the process of sending email reports to specific recipients.

---
+++ FUNCTIONS/prepare_mail.py
@@ -0,0 +1,37 @@
+from PRIVATE import send_mail
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def run(path_to_file, file_name, html_file):
+    """
+        Prepares and sends an email. The email server and recipients are specified using
+        environment variables ("main_mail", "store_mail" and "subject").
+
+        The mail content includes a HTML fixture and attachment file.
+
+        Args:
+            path_to_file (str): The path to the attachment file.
+            file_name (str): The name of the attachment file.
+            html_file (str): The path to a HTML file for the email content body.
+
+        Note:
+            The environment variables "main_mail", "store_mail" and "subject"
+            should be set before running this function.
+    """
+
+    mail_lst = [os.getenv("main_mail"),
+                # os.getenv("store_mail")
+                ]
+    mail_names = [os.getenv("subject"),
+                  # os.getenv("subject"),
+                ]
+
+    # -------------------- OPEN HTML FILE FOR THE BODY MAIL --------------------
+    with open(html_file, 'r') as html_file:
+        word = html_file.read()
+
+    # -------------------- SEND MAIL --------------------
+    send_mail.send_mail(mail_lst, mail_names, word, path_to_file, file_name)
