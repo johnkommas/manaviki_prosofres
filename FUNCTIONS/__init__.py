f95eaca0600199c806092a0964e1c1cace757315
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2023-10-30 22:45:03 +0200

    Add FUNCTION/__init__.py to import functions
    This commit adds an __init__.py file under the FUNCTIONS directory. This allows for easier referencing of all functions used in the project. Specifically, the functions imported are from excel, find_price_changes, prepare_mail, and read_prosfores modules. This change was made for code modularity and better organization.

---
+++ FUNCTIONS/__init__.py
@@ -0,0 +1,4 @@
+from .excel import export
+from .find_price_changes import run_gresko, run_kalimera
+from .prepare_mail import run
+from .read_prosfores import run_prosfores_gresko, run_prosfores_kalimera
