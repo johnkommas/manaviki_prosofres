795a164d1918906815c47662ea0578a5a9406ffe
Ioannis E. Kommas <54149422+johnkommas@users.noreply.github.com>
2023-10-31 13:56:01 +0200

    Refactor variable names and disable price comparison
    Variable names related to the date lists have been refactored for 'GRESKO' and 'KALIMERA FROUTA' to improve the clarity and readability of the code. Also, the functionality that compares the old and new prices for both has been temporarily disabled, probably due to some issues that need to be resolved, as evident from these lines being commented out. The further step of codebase optimization could be resetting these processes after troubleshooting and debugging.

--- main.py
+++ main.py
@@ -27,27 +27,33 @@ def run():
     path_to_file = f'{cwd}/OUTPUT/{file_name}'
     html_file = f'{cwd}/HTML/body.html'
 
-    list_of_dates =['26102023', '30102023']
-    new = list_of_dates[-1]
-    old = list_of_dates[-2]
+    list_of_dates_gresco =['26102023', '30102023']
+    new_gresco = list_of_dates_gresco[-1]
+    # old_gresco = list_of_dates_gresco[-2]
+
+    list_of_dates_kalimera = ['26102023', '30102023']
+    new_kalimera = list_of_dates_kalimera[-1]
+    # old_kalimera = list_of_dates_kalimera[-2]
 
     # NEW PRICES GRESKO | A. FIND FILE | B. CREATE DATAFRAME
-    fa = f'{cwd}/FILES/GRESKO {new}.xlsx'
+    fa = f'{cwd}/FILES/GRESKO {new_gresco}.xlsx'
     gresco = myfunc.run_prosfores_gresko(fa)
 
     # NEW PRICES KALIMERA FROUTA | A. FIND FILE | B. CREATE DATAFRAME
-    fb = f'{cwd}/FILES/FNV {new}.pdf'
+    fb = f'{cwd}/FILES/FNV {new_kalimera}.pdf'
     kalimera_frouta = myfunc.run_prosfores_kalimera(fb)
 
     # OLD PRICES GRESKO | A.FIND FILE | B.CREATE DATAFRAME | C.COMPARE WITH NEW PRICES
-    fc = f'{cwd}/FILES/GRESKO {old}.xlsx'
-    gresco_old = myfunc.run_prosfores_gresko(fc)
-    gresco_price_change = myfunc.run_gresko(gresco, gresco_old)
+    gresco_price_change = None
+    # fc = f'{cwd}/FILES/GRESKO {old_gresco}.xlsx'
+    # gresco_old = myfunc.run_prosfores_gresko(fc)
+    # gresco_price_change = myfunc.run_gresko(gresco, gresco_old)
 
     # OLD PRICES KALIMERA FROUTA | A.FIND FILE | B.CREATE DATAFRAME | C.COMPARE WITH NEW PRICES
-    fd = f'{cwd}/FILES/FNV {old}.pdf'
-    kalimera_frouta_old = myfunc.run_prosfores_kalimera(fd)
-    kalimera_price_changed = myfunc.run_kalimera(kalimera_frouta, kalimera_frouta_old)
+    kalimera_price_changed = None
+    # fd = f'{cwd}/FILES/FNV {old_kalimera}.pdf'
+    # kalimera_frouta_old = myfunc.run_prosfores_kalimera(fd)
+    # kalimera_price_changed = myfunc.run_kalimera(kalimera_frouta, kalimera_frouta_old)
 
     # CREATE EXCEL FUNCTION
     myfunc.export(f'{cwd}/OUTPUT/manaviki.xlsx', gresco, kalimera_frouta, gresco_price_change, kalimera_price_changed)
